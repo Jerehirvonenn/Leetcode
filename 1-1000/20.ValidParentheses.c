c3b9d7f2d232f96baf4ade69c687c97151c74eae
jerehirvonenn <jerehirvonen21@gmail.com>
2025-01-07 14:30:22 +0200

    adding 20. Valid Parentheses in c

---
+++ 1-1000/20.ValidParentheses.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isMatchingPair(char open, char close)
+{
+    return (open == '(' && close == ')') ||
+           (open == '{' && close == '}') ||
+           (open == '[' && close == ']');
+}
+
+bool isValid(char* s)
+{
+    int     size = strlen(s);
+    if (size % 2 != 0)
+        return false;
+    char    *stack = (char*)malloc(sizeof(char) * size);
+    int     top = -1;
+    for (int i = 0; i < size; i++)
+    {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            stack[++top] = s[i];
+        else
+        {
+            if (top == -1 || !isMatchingPair(stack[top], s[i]))
+            {
+                free(stack);
+                return false;
+            }
+            top--;
+        }
+    }
+    bool valid = (top == -1);
+    free(stack);
+    return valid;
+}
+
+int	main(int ac, char **av)
+{
+	if (ac < 2)
+	{
+		printf("provide at least 1 argument\n");
+		return 1;
+	}
+	bool result = isValid(av[1]);
+	if (result)
+		printf("valid\n");
+	else
+		printf("invalid\n");
+}
