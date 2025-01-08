fc89c73eb619a91ff004d99a46a676e6db601452
jerehirvonenn <jerehirvonen21@gmail.com>
2025-01-08 14:30:17 +0200

    27. in c

---
+++ 1-1000/27.RemoveElement.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	removeElement(int* nums, int numsSize, int val)
+{
+	int i = 0;
+	for (int j = 0; j < numsSize; j++)
+		if (nums[j] != val)
+			nums[i++] = nums[j];
+	return i;
+}
+
+int	main(int ac, char **av)
+{
+	if (ac < 2)
+		return (1);
+
+	int	array[ac - 1];
+	for (int i = 1; i < ac; i++)
+		array[i - 1] = atoi(av[i]);
+	int size = removeElement(array, ac - 1, 3);
+	for (int i = 0; i < size; i++)
+		printf("%d ", array[i]);
+	printf("\n");
+}
