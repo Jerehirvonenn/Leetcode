4260f72dd04c21f8bb451abde4314cdb53c84d72
jerehirvonenn <jerehirvonen21@gmail.com>
2025-01-10 15:05:01 +0200

    testing clang format

--- 1-1000/20.ValidParentheses.c
+++ 1-1000/20.ValidParentheses.c
@@ -1,42 +1,41 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
 bool isMatchingPair(char open, char close)
 {
-    return (open == '(' && close == ')') ||
-           (open == '{' && close == '}') ||
-           (open == '[' && close == ']');
+	return (open == '(' && close == ')') || (open == '{' && close == '}') ||
+		   (open == '[' && close == ']');
 }
 
-bool isValid(char* s)
+bool isValid(char *s)
 {
-    int     size = strlen(s);
-    if (size % 2 != 0)
-        return false;
-    char    *stack = (char*)malloc(sizeof(char) * size);
-    int     top = -1;
-    for (int i = 0; i < size; i++)
-    {
-        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
-            stack[++top] = s[i];
-        else
-        {
-            if (top == -1 || !isMatchingPair(stack[top], s[i]))
-            {
-                free(stack);
-                return false;
-            }
-            top--;
-        }
-    }
-    bool valid = (top == -1);
-    free(stack);
-    return valid;
+	int size = strlen(s);
+	if (size % 2 != 0)
+		return false;
+	char *stack = (char *)malloc(sizeof(char) * size);
+	int top = -1;
+	for (int i = 0; i < size; i++)
+	{
+		if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+			stack[++top] = s[i];
+		else
+		{
+			if (top == -1 || !isMatchingPair(stack[top], s[i]))
+			{
+				free(stack);
+				return false;
+			}
+			top--;
+		}
+	}
+	bool valid = (top == -1);
+	free(stack);
+	return valid;
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
 	if (ac < 2)
 	{
--- 1-1000/21.MergeTwoSortedLists.c
+++ 1-1000/21.MergeTwoSortedLists.c
@@ -1,65 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct	ListNode
+struct ListNode
 {
-    int			val;
-    struct ListNode*	next;
+	int val;
+	struct ListNode *next;
 };
 
-struct ListNode *mergeTwoLists(struct ListNode *list1,struct ListNode *list2)
+struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2)
 {
-    struct ListNode    tmp;
-    struct ListNode    *tail = &tmp;
+	struct ListNode tmp;
+	struct ListNode *tail = &tmp;
 
-    tmp.next = NULL;
-    while (list1 && list2)
-    {
-        if (list1->val <= list2->val)
-        {
-            tail->next = list1;
-            list1 = list1->next;
-        }
-        else
-        {
-            tail->next = list2;
-            list2 = list2->next;
-        }
-        tail = tail->next;
-    }
-    tail->next = (list1 != NULL) ? list1 : list2;
-    return tmp.next;
+	tmp.next = NULL;
+	while (list1 && list2)
+	{
+		if (list1->val <= list2->val)
+		{
+			tail->next = list1;
+			list1 = list1->next;
+		}
+		else
+		{
+			tail->next = list2;
+			list2 = list2->next;
+		}
+		tail = tail->next;
+	}
+	tail->next = (list1 != NULL) ? list1 : list2;
+	return tmp.next;
 }
 
-struct ListNode* createNode(int val)
+struct ListNode *createNode(int val)
 {
-    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
-    newNode->val = val;
-    newNode->next = NULL;
-    return newNode;
+	struct ListNode *newNode =
+		(struct ListNode *)malloc(sizeof(struct ListNode));
+	newNode->val = val;
+	newNode->next = NULL;
+	return newNode;
 }
 
-void printList(struct ListNode* head)
+void printList(struct ListNode *head)
 {
-    while (head)
-    {
-        printf("%d -> ", head->val);
-        head = head->next;
-    }
-    printf("NULL\n");
+	while (head)
+	{
+		printf("%d -> ", head->val);
+		head = head->next;
+	}
+	printf("NULL\n");
 }
 
 int main(void)
 {
-    struct ListNode* l1 = createNode(1);
-    l1->next = createNode(2);
-    l1->next->next = createNode(4);
+	struct ListNode *l1 = createNode(1);
+	l1->next = createNode(2);
+	l1->next->next = createNode(4);
 
-    struct ListNode* l2 = createNode(1);
-    l2->next = createNode(3);
-    l2->next->next = createNode(4);
+	struct ListNode *l2 = createNode(1);
+	l2->next = createNode(3);
+	l2->next->next = createNode(4);
 
-    struct ListNode* mergedList = mergeTwoLists(l1, l2);
+	struct ListNode *mergedList = mergeTwoLists(l1, l2);
 
-    printList(mergedList);
+	printList(mergedList);
 }
