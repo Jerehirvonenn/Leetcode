5465aa75333f5480911412f9c358027a7065e8cb
jerehirvonenn <jerehirvonen21@gmail.com>
2025-01-12 22:13:12 +0200

    adding 58. in c

---
+++ 1-1000/58.LengthOfLastWord.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
+int lengthOfLastWord(char* s)
+{
+    int end = strlen(s) - 1;
+    while (end >= 0 && isspace(s[end]))
+        end--;
+    int i = 0;
+    while (end >= 0 && isalpha(s[end]))
+    {
+        end--;
+        i++;
+    }
+    return i;
+}
+
+int	main(int ac, char **av)
+{
+	if (ac < 2)
+		return 1;
+	printf("Last word length is %d\n", lengthOfLastWord(av[1]));
+}
