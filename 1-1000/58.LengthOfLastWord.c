bfcf23a7d562ba052e04aa714c8593ba080990ab
jerehirvonenn <jerehirvonen21@gmail.com>
2025-01-12 22:16:24 +0200

    58 fixed syntax

--- 1-1000/58.LengthOfLastWord.c
+++ 1-1000/58.LengthOfLastWord.c
@@ -1,22 +1,22 @@
 #include <ctype.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
-int lengthOfLastWord(char* s)
+int lengthOfLastWord(char *s)
 {
-    int end = strlen(s) - 1;
-    while (end >= 0 && isspace(s[end]))
-        end--;
-    int i = 0;
-    while (end >= 0 && isalpha(s[end]))
-    {
-        end--;
-        i++;
-    }
-    return i;
+	int end = strlen(s) - 1;
+	while (end >= 0 && isspace(s[end]))
+		end--;
+	int i = 0;
+	while (end >= 0 && isalpha(s[end]))
+	{
+		end--;
+		i++;
+	}
+	return i;
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
 	if (ac < 2)
 		return 1;
