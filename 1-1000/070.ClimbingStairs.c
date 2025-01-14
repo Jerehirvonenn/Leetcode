3a583c0f7233112d7aadff6cae493b9c1c2dd37b
jerehirvonenn <jerehirvonen21@gmail.com>
2025-01-14 10:41:59 +0200

    adding 70. in c

---
+++ 1-1000/070.ClimbingStairs.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int climbStairs(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    int first = 1;
+    int second = 2;
+    for (int i = 3; i <= n; i++)
+    {
+        int current = first + second;
+        first = second;
+        second = current;
+    }   
+    return second; 
+}
+
+int main(int ac, char **av)
+{
+    if (ac < 2)
+    {
+        printf("Usage: ./a.out <number>\n");
+        return 1;
+    }
+    int num = atoi(av[1]);
+    if (num < 0)
+    {
+        printf("provide a non negative number\n");
+        return 1;
+    }
+    int result = climbStairs(num);
+    printf("There are %d distinct ways to climb %d steps.\n", result, num);
+}
