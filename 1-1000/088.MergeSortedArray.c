2e8e33ac9aa2af7ffa8336ce33de85fbda8831a2
jerehirvonenn <jerehirvonen21@gmail.com>
2025-01-25 23:51:25 +0200

    small test main for 88

--- 1-1000/088.MergeSortedArray.c
+++ 1-1000/088.MergeSortedArray.c
@@ -16,3 +16,18 @@ void merge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size, int n)
 	while (index2 >= 0)
 		nums1[toPut--] = nums2[index2--];
 }
+
+int main(void)
+{
+	int nums1[10] = {1, 3, 5, 7, 9};
+	int nums2[5] = {2, 4, 6, 8, 10};
+	int m = 5;
+	int n = 5;
+
+	merge(nums1, 10, m, nums2, 5, n);
+
+	printf("Merged array: ");
+	for (int i = 0; i < m + n; i++)
+		printf("%d ", nums1[i]);
+	printf("\n");
+}
