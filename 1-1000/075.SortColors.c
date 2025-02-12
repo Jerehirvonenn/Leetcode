9f8d2fd248f384ddb9a6bc82541f1793f828f7e2
jerehirvonenn <jerehirvonen21@gmail.com>
2025-02-12 22:53:21 +0200

    small tweak to 75 (clang format)

--- 1-1000/075.SortColors.c
+++ 1-1000/075.SortColors.c
@@ -3,35 +3,35 @@
 
 void swap(int *a, int *b)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
-void sortColors(int* nums, int numsSize)
+void sortColors(int *nums, int numsSize)
 {
-    int zero = 0;
-    int two = numsSize - 1;
-    int i = zero;
-    while (i <= two)
-    {
-        if (nums[i] == 0)
-        {
-            swap(&(nums[i]), &(nums[zero]));
-            i++;
-            zero++;
-        }
-        else if (nums[i] == 2)
-        {
-            swap(&(nums[i]), &(nums[two]));
-            two--;
-        }
-        else
-            i++;
-    }
+	int zero = 0;
+	int two = numsSize - 1;
+	int i = zero;
+	while (i <= two)
+	{
+		if (nums[i] == 0)
+		{
+			swap(&(nums[i]), &(nums[zero]));
+			i++;
+			zero++;
+		}
+		else if (nums[i] == 2)
+		{
+			swap(&(nums[i]), &(nums[two]));
+			two--;
+		}
+		else
+			i++;
+	}
 }
 
-void	print_array(char *str, int *array, int size)
+void print_array(char *str, int *array, int size)
 {
 	printf("%s", str);
 	for (int i = 0; i < size; i++)
@@ -43,7 +43,8 @@ int main(int ac, char **av)
 {
 	if (ac < 2 || ac > 101)
 	{
-		printf("Usage: %s <num1> <num2> ... <numN> || N < 100 0 <= num <= 2\n", av[0]);
+		printf("Usage: %s <num1> <num2> ... <numN> || N < 100 0 <= num <= 2\n",
+			   av[0]);
 		return (1);
 	}
 	ac--;
@@ -54,7 +55,9 @@ int main(int ac, char **av)
 		array[i] = atoi(av[i]);
 		if (array[i] > 2 || array[i] < 0)
 		{
-			printf("Usage: %s <num1> <num2> ... <numN> || N < 100 0 <= num <= 2\n", av[0]);
+			printf(
+				"Usage: %s <num1> <num2> ... <numN> || N < 100 0 <= num <= 2\n",
+				av[0]);
 			return (2);
 		}
 	}
