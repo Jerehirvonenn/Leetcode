da3dcf3073dc4b924e88c2efac673e8a2ca28fa5
jerehirvonenn <jerehirvonen21@gmail.com>
2025-03-15 12:52:22 +0200

    adding 27 in c++

---
+++ c++/0027.RemoveElement.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    static int removeElement(vector<int>& nums, int val)
+	{
+		int left = 0;
+		for (unsigned int right = 0; right < nums.size(); right++) {
+			if (nums[right] != val) {
+				nums[left++] = nums[right];
+			}
+		}
+		return left;
+    }
+};
+
+int main(int ac, char **av)
+{
+	if (ac < 2)
+		return 1;
+	ac--;
+	av++;
+	try
+	{
+		int	target = 3;
+		vector<int> nums;
+		nums.reserve(ac);
+		for (int i = 0; i < ac; i++) {
+			nums.push_back(stoi(av[i]));
+		}
+		int result = Solution::removeElement(nums, target);
+		cout << "Vector after: ";
+		for (unsigned int i = 0; i < result; i++) {
+			cout << nums[i];
+		}
+		cout << endl;
+	}
+	catch (exception &e)
+	{
+		cout << e.what() << endl;
+	}
+}
