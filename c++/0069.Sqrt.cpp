694785d7c4307468b12afc766821146a5d440409
jerehirvonenn <jerehirvonen21@gmail.com>
2025-04-13 23:33:04 +0300

    69 in c++, syntax

--- c++/0069.Sqrt.cpp
+++ c++/0069.Sqrt.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
 
-class Solution {
-public:
-    int mySqrt(int x)
+class Solution
+{
+	public:
+    static int mySqrt(int x)
 	{
-		if (x == 0 || x == 1) {
+		if (x == 0 || x == 1)
 			return x;
-		}
+
 		int left = 0;
 		int right = x / 2;
 		while (left < right)
 		{
 			int middle = left + (right - left) / 2;
 			long long result = static_cast<long long>(middle) * static_cast<long long>(middle);
-			if (result == x) {
+			if (result == x)
 				return middle;
-			}
-			else if (result > x) {
+			else if (result > x)
 				right = middle;
-			}
-			else {
+			else
 				left = middle + 1;
-			}
 		}
 		if (static_cast<long long>(left) * static_cast<long long>(left) > x)
 			return left - 1;
@@ -34,7 +32,6 @@ int main(int ac, char **av)
 	if (ac < 2)
 		return 1;
 	int input = atoi(av[1]);
-	Solution solve;
-	int result = solve.mySqrt(input);
+	int result = Solution::mySqrt(input);
 	std::cout << result << std::endl;
 }
