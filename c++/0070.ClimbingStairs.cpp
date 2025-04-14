6c4cb2e723f787558bd5c063e6650812f504b87e
jerehirvonenn <jerehirvonen21@gmail.com>
2025-04-14 20:13:27 +0300

    70 in c++

---
+++ c++/0070.ClimbingStairs.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Solution
+{
+	public:
+    int climbStairs(int n)
+	{
+		if (n == 1 || n == 2 || n == 3) {
+			return n;
+		}
+
+		int current = 3;
+		int prev = 2;
+		int total = 0;
+		for (int i = 3; i < n; i++) {
+			total = prev + current;
+			prev = current;
+			current = total;
+		}
+		return total;
+    }
+};
+
+int main(int ac, char **av)
+{
+	if (ac < 2)
+		return 1;
+
+	int stairs = atoi(av[1]);
+	Solution solve;
+	int result = solve.climbStairs(stairs);
+	std::cout << result << std::endl;
+}
