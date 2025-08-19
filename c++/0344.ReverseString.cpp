672ccadedb96b3da1b197a4bf6fa5177916d33b5
jerehirvonenn <jerehirvonen21@gmail.com>
2025-08-19 23:21:36 +0300

    344 c++

--- c++/0344.ReverseString.cpp
+++ c++/0344.ReverseString.cpp
@@ -20,9 +20,8 @@ public:
     void reverseString(vector<char>& s) {
         int left = 0;
         int right = s.size() - 1;
-        while (left < right) {
+        while (left < right)
             swap(s[left++], s[right--]);
-        }
     }
 };
 
