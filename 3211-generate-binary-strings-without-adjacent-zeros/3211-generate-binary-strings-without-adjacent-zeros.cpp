c4032cccd6492c1acb3da2740157dd1f6e93fe99
Jere <162603246+Jerehirvonenn@users.noreply.github.com>
2025-12-15 15:51:44 +0200

    Time: 12 ms (68.48%), Space: 18 MB (91.54%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,25 +1,20 @@
 class Solution {
 private:
-    void    buildSubStrs(vector<string> &result, string &s, int n) {
-        if (s.length() >= n) {
-            result.push_back(s);
-            return ;
-        }
-
-        if (s.empty() || s.back() == '1') {
-            s += '0';
-            buildSubStrs(result, s, n);
-            s.pop_back();
-        }
-        s += '1';
-        buildSubStrs(result, s, n);
-        s.pop_back();
-    }
 public:
     vector<string> validStrings(int n) {
         vector<string> result;
-        string current = "";
-        buildSubStrs(result, current, n);
+        result.push_back("");
+
+        for (int i = 0; i < n; i++) {
+            vector<string> tmp;
+            for (const string s : result) {
+                tmp.push_back(s + '1');
+                if (s.empty() || s.back() == '1') {
+                    tmp.push_back(s + '0');
+                }
+            }
+            result = move(tmp);
+        }
         return result;
     }
 };
