1a57a0fc2014d6d6ff0fb20bce2fdbe1c62cc8da
Jere <162603246+Jerehirvonenn@users.noreply.github.com>
2025-12-15 15:53:25 +0200

    Time: 12 ms (68.48%), Space: 18.1 MB (91.33%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
@@ -3,13 +3,16 @@ private:
 public:
     vector<string> validStrings(int n) {
         vector<string> result;
-        result.push_back("");
+        if (n == 0)
+            return result;
+        result.push_back("1");
+        result.push_back("0");
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             vector<string> tmp;
             for (const string s : result) {
                 tmp.push_back(s + '1');
-                if (s.empty() || s.back() == '1') {
+                if (s.back() == '1') {
                     tmp.push_back(s + '0');
                 }
             }
