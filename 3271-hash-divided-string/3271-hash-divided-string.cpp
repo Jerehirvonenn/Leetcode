98aae4f0560c9c3c050cc2e8b1a84e92017d9ccb
Jere <162603246+Jerehirvonenn@users.noreply.github.com>
2026-01-20 16:37:05 +0200

    Time: 7 ms (13.73%), Space: 11.2 MB (60.13%) - LeetHub

---
+++ 3271-hash-divided-string/3271-hash-divided-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string stringHash(string s, int k) {
+        int n = s.size();
+        string result;
+        result.reserve(n / 3);
+
+        int hashValue = 0;
+        for (int i = 0; i < n; i++) {
+            if (i != 0 && i % k == 0) {
+                result += static_cast<char>((hashValue % 26) + 'a');
+                hashValue = 0;
+            }
+            hashValue += s[i] - 'a';
+        }
+        result += static_cast<char>((hashValue % 26) + 'a');
+        return result;
+    }
+};
