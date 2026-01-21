f4fba9188a7ef47552df5d8fc014d3296f44e897
Jere <162603246+Jerehirvonenn@users.noreply.github.com>
2026-01-21 21:37:59 +0200

    Time: 17 ms (9.08%), Space: 19.1 MB (47.63%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -1,11 +1,10 @@
 class Solution {
-    inline bool isVowel(char c) {
-        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-    }
 public:
     bool doesAliceWin(string s) {
         for (const char c : s) {
-            if (isVowel(c)) { return true; }
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                return true;
+            }
         }
         return false;
     }
