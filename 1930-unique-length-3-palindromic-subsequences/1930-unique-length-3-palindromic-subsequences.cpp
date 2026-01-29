42a05bd844290a1e5c182c38b9eedbf7ed581a4e
Jere <162603246+Jerehirvonenn@users.noreply.github.com>
2026-01-29 16:43:03 +0200

    Time: 23 ms (89.83%), Space: 14 MB (86.56%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    int uniquesBetweenIndexes(int start, int end, string &s) {
+        bitset<26> seen;
+        int result = 0;
+        for (int i = start + 1; i < end; i++) {
+            int current = s[i] - 'a';
+            if (seen[current])
+                continue ;
+            result++;
+            seen[current].flip();
+        }
+        return result;
+    }
+public:
+    int countPalindromicSubsequence(string s) {
+        int n = s.size();
+        vector<pair<int,int>> idx(26, pair<int,int>(-1, -1)); //first and last index of char
+
+        for (int i = 0; i < n; i++) {
+            int current = s[i] - 'a';
+            if (idx[current].first == -1) { //finding char first time
+                idx[current].first = i;
+            }
+            idx[current].second = i;
+        }
+
+        int result = 0;
+        for (const auto &[first, last] : idx) {
+            result += uniquesBetweenIndexes(first, last, s);
+        }
+        return result;
+    }
+};
