59217f3a94eb567415c2fc409521d94c022f50bc
Jere <162603246+Jerehirvonenn@users.noreply.github.com>
2026-02-03 20:30:59 +0200

    Time: 0 ms (100%), Space: 11.1 MB (99.29%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -1,12 +1,4 @@
 class Solution {
-private:
-    int binaryToInt(vector<int> num) {
-        int result = 0;
-        for (int i = 0; i < num.size(); i++) {
-            result = (result * 2) + num[i];
-        }
-        return result;
-    }
 public:
     int matrixScore(vector<vector<int>>& grid) {
         int rows = grid.size();
@@ -14,39 +6,15 @@ public:
             return 0;
         int cols = grid[0].size();
 
-        //check rows start with 1
-        for (int i = 0; i < rows; i++) {
-            if (grid[i][0] == 0) {
-                for (int j = 0; j < cols; j++) {
-                    grid[i][j] = (grid[i][j] + 1) % 2;
-                }
-            }
-        }
-
-        //if col has more 0 than 1, greedy flip
-        for (int col = 0; col < cols; col++) {
-            int balance = 0;
+        int result = rows * (1 << (cols - 1));
+        for (int col = 1; col < cols; col++) {
+            int ones = 0;
             for (int row = 0; row < rows; row++) {
-                if (grid[row][col] == 0)
-                    balance--;
-                else 
-                    balance++;
+                if (grid[row][0] == grid[row][col])
+                    ones++;
             }
-
-            if (balance >= 0)
-                continue ;
-            
-            for (int row = 0; row < rows; row++) {
-                if (grid[row][col] == 0)
-                    grid[row][col] = 1;
-                else 
-                    grid[row][col] = 0;
-            }
-        }
-
-        int result = 0;
-        for (int row = 0; row < rows; row++) {
-            result += binaryToInt(grid[row]);
+            ones = max(ones, rows - ones);
+            result += ones * (1 << (cols - col - 1));
         }
         return result;
     }
